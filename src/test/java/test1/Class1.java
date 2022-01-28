99e07199225b85c5ca25efb809004aa6243daed1
Rajkumar7444 <rajkumarpatil017@gmail.com>
2022-01-28 13:59:20 +0530

    First changes

---
+++ src/test/java/test1/Class1.java
@@ -0,0 +1,14 @@
+package test1;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+	
+	@Test
+	
+	public void FirstTest()
+	{
+		System.out.println("It's a first test");
+	}
+
+}
---
+++ src/test/java/test1/Class2.java
@@ -0,0 +1,13 @@
+package test1;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+@Test
+	
+	public void SecondTest()
+	{
+		System.out.println("It's a second test");
+	}
+}
